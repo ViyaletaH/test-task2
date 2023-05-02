7e9a4d31b031ef03afa097f9f4d1c9ea7595cc9f
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-05-02 10:45:31 +0200

    feat: add nav-line styles

--- figma-task/src/components/NavLine.tsx
+++ figma-task/src/components/NavLine.tsx
@@ -1,6 +1,13 @@
 
 const NavLine = () => {
-  return <div>nav line</div>;
+  return <div className="nav-line">
+  <div className="home">
+    <img src="/public/home.png" alt="home" width="19px" height="19px"/>
+  </div>
+  <span className="startseite">Startseite</span>
+  <span className="nav-arrow">&gt;</span>
+  <span className="current">Uber uns</span>
+</div>;
 };
 
 export default NavLine;
