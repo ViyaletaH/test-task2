97d9f01a4fabcedb06f2c88b9123d9ed4c680f6f
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-05-07 09:10:46 +0200

    feat: mobile header, nav-line styles

--- figma-task/src/components/NavLine.tsx
+++ figma-task/src/components/NavLine.tsx
@@ -4,14 +4,14 @@ const NavLine = () => {
   return (
     <div className="nav-line">
       <div className="home">
-        <img src="/home.png" alt="home" width="20px" height="20px" />
+        <img src="/home.png" alt="home" width="20px" height="20px" className="home" />
       </div>
       <Link to="/start">
         <span className="startseite">Startseite</span>
       </Link>
       <span className="nav-arrow">&gt;</span>
       <Link to="/">
-      <span className="current">Uber uns</span>
+        <span className="current">Uber uns</span>
       </Link>
     </div>
   );
